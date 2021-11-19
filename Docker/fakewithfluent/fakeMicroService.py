5a0e4b4ce3ecf72eb0efa24e6f62010deac10464
Fuad <fujam20@student.sdu.dk>
2021-11-19 15:52:21 +0100

    fixed gensong not being printed in fakemicroservice

--- Docker/fakewithfluent/fakeMicroService.py
+++ Docker/fakewithfluent/fakeMicroService.py
@@ -55,7 +55,7 @@ for _ in range(5):
 
 #Creates events in a loop
 while True:
-    rand = random.randint(1,8)
+    rand = random.randint(1,9)
     time.sleep(rand)
 
     
@@ -82,9 +82,9 @@ while True:
         entry = generate_songPausedAndUnpaused(users,songs,days)
     elif switch == 7:
         entry = generate_adClicks(users,days)
-    elif switch == 7:
-        entry = gen_song(days)
     elif switch == 8:
+        entry = gen_song(days,artists)
+    elif switch == 9:
         entry = gen_artist(days)
         
 
