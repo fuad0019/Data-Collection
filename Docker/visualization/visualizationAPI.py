8708b137dc03545169d714cffc889a20f73add0c
mnhnielsen <mnhnielsen@gmail.com>
2021-12-17 11:18:47 +0100

    visualization + user pull test
    pull users when visuals/user is called instead of manual pull via data.puller endpoints.

--- Docker/visualization/visualizationAPI.py
+++ Docker/visualization/visualizationAPI.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template  
+import datapuller
 
 # This sets up the application using the Flask object from the package flask.
 app = Flask(__name__)
@@ -11,6 +12,7 @@ def home():
 
 @app.route('/user')
 def getUsers():
+    datapuller.get_users()
     return render_template("index.html")
 
 
